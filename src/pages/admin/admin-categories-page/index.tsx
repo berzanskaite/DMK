8a87495b71f028687daf02596735b48e958a5496
fcmilda <milda.berzanskaite@gmail.com>
2022-06-14 20:17:06 +0300

    feat: create new category

--- src/pages/admin/admin-categories-page/index.tsx
+++ src/pages/admin/admin-categories-page/index.tsx
@@ -1,16 +1,51 @@
 import React, { useEffect } from 'react';
-import { Button, Container, Typography } from '@mui/material';
+import {
+  Button, Container, Typography, TextField, Paper, Box,
+} from '@mui/material';
+
+import { useFormik, FormikConfig } from 'formik';
 import SectionTitle from 'components/sectiontitle';
 import { useRootSelector } from 'store';
 import { selectCategories, selectCategoriesLoading } from 'store/selectors';
+import { CreateCategory } from 'types';
+import * as Yup from 'yup';
 import { useRootDispatch } from '../../../store/hooks';
-import { categoriesFetchCategoriesAction, createCategoriesDeleteCategoryAction } from '../../../store/features/categories/action-creators';
+import { categoriesFetchCategoriesAction, createCategoriesCreateNewCategoryAction, createCategoriesDeleteCategoryAction } from '../../../store/features/categories/action-creators';
 import CategoryListItem from './category-list-item';
+
+type CreateCategoryFormikConfig = FormikConfig<CreateCategory>;
+
+const validationSchema = Yup.object({
+  title: Yup.string()
+    .required('Privalomas laukas'),
+});
 
 const CategoriesPage: React.FC = () => {
   const categories = useRootSelector(selectCategories);
   const categoriesLoading = useRootSelector(selectCategoriesLoading);
   const dispatch = useRootDispatch();
+
+  const initialValues = {
+    title: '',
+  };
+
+  const handleSubmitForm: CreateCategoryFormikConfig['onSubmit'] = (category) => {
+    const createAction = createCategoriesCreateNewCategoryAction(category);
+    dispatch(createAction);
+  };
+
+  const {
+    values,
+    touched,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = useFormik<CreateCategory>({
+    initialValues,
+    onSubmit: handleSubmitForm,
+    validationSchema,
+  });
 
   useEffect(() => {
     dispatch(categoriesFetchCategoriesAction);
@@ -40,11 +75,47 @@ const CategoriesPage: React.FC = () => {
 
   return (
     <Container sx={{
-      display: 'flex', flexDirection: 'column', mb: 5, height: '100vh',
+      display: 'flex', flexDirection: 'column', mb: 5,
     }}
     >
       <SectionTitle title="Kategorijos" description="Kategorijų tvarkymo puslapis" />
       {content}
+
+      <SectionTitle title="Kurti naują kategoriją" description="" />
+      <Box
+        component="form"
+        sx={{
+          display: 'flex',
+          mx: 'auto',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 1,
+          p: 3,
+          width: 400,
+        }}
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          name="title"
+          type="text"
+          label="Pavadinimas"
+          fullWidth
+          inputProps={{ autoComplete: 'off' }}
+          value={values.title}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.title && Boolean(errors.title)}
+          helperText={touched.title && errors.title}
+        />
+        <Button
+          variant="contained"
+          size="large"
+          type="submit"
+          sx={{ width: '120px' }}
+        >
+          Sukurti
+        </Button>
+      </Box>
       <Button href="/admin">Grįžti į administratoriaus puslapį</Button>
     </Container>
   );
@@ -54,8 +125,8 @@ export default CategoriesPage;
 /*
 Liko padaryti:
 kad kategorijos issitrintu is saraso, ne tik is duombazes
-kategoriju redagavimas, ne tik trynimas
-nauju kategoriju ivedimas
+kad kategorija atsirastu sarase, ne tik duombazej
+kategoriju redagavimas
 
 kad sudejus kategorijas i createNewItem, jos isirasytu i duombaze(su Irma)
 padaryti kategoriju redagavima changeItem formoje
